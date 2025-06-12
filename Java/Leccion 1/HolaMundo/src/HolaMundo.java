348a8f29b9f9e5f4fd70b33b3b6c5cb0188bab81
Abril Cisterna <abril.cisterna23@gmail.com>
2025-06-12 02:26:24 -0300

    Se agrego la Leccion 1 de Java, incluyendo el proyecto Hola Mundo, junto con el contenido de la clase 2

---
+++ Java/Leccion 1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,16 @@
+//Nuestro primer programa Hola mundo
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo, desde Java");
+        
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        // Tipo String
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programacion";
+        System.out.println(miVariableCadena);
+    }
+}
