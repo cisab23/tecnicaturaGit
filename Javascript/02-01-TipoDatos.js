6dbc14b8706c9950b7bc57d306a4bc080ceb875a
Abril Cisterna <abril.cisterna23@gmail.com>
2025-06-25 16:32:12 -0300

    Se añade la clase 3 de JavaScript

---
+++ Javascript/02-01-TipoDatos.js
@@ -0,0 +1,20 @@
+// Tipos de Datos en Javascript
+/* La sintaxis en lo que es comentarios
+es muy similar a la de Java
+realmente diriamos que es identica*/
+
+// Tipo Str
+var nombre = 'Abril';
+console.log(nombre)
+
+// Tipo Numerico
+var numero = 3000;
+console.log(numero);
+
+// Tipo Object
+var objeto = {
+    nombre: "Abril",
+    apellido: "Cisterna",
+    telefono: "030345678"
+}
+console.log(objeto)
