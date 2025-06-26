f350bc11ffd6e4203d86d44b45d09820ab06ea1e
Abril Cisterna <abril.cisterna23@gmail.com>
2025-06-26 00:35:53 -0300

    Se agrego la teoria de la clase 7 de JavaScript

--- Javascript/leccion02 - clase 3 a 6/02-01-ConcatenacionDeCadenas.js
+++ Javascript/leccion02 - clase 3 a 6/02-01-ConcatenacionDeCadenas.js
@@ -14,3 +14,19 @@ console.log(juntos);
 
 nombre += apellido; //Operador simplificado
 console.log(nombre);
+
+// Clase 7
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+//apellido2 = "Peres"; una constante no puede ser modificada
+console.log(apellido2);
+
+let x, y; //Se pueden crear varias variables dentro de una misma linea
+x = 17, y = 21; //Se pueden asignar varias variables dentro de una misma linea
+let z = x + y; //Se asigna el valor de la operacion
+console.log(z);
+
+// let _1num = 31; No utilizar numeros para iniciar el nombre de una variable
+// let break = "rompe"; No utilizar palabras reservadas para variables
